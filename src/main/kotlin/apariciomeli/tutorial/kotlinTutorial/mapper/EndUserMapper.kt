c497a30a067edcb8f04d16a07bbd3fedacd6cdc3
aparicioFranco <francoapariciom@gmail.com>
2024-09-20 15:10:13 -0300

    Changed registerUser.

--- src/main/kotlin/apariciomeli/tutorial/kotlinTutorial/mapper/EndUserMapper.kt
+++ src/main/kotlin/apariciomeli/tutorial/kotlinTutorial/mapper/EndUserMapper.kt
@@ -4,15 +4,18 @@ import apariciomeli.tutorial.kotlinTutorial.dto.user.EndUserDTO
 import apariciomeli.tutorial.kotlinTutorial.model.EndUser
 import org.springframework.stereotype.Service
 import apariciomeli.tutorial.kotlinTutorial.model.Role
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @Service
-class EndUserMapper: Mapper<EndUserDTO, EndUser> {
+class EndUserMapper(
+    val passwordEncoder: PasswordEncoder
+): Mapper<EndUserDTO, EndUser> {
     override fun fromEntity(entity: EndUser): EndUserDTO {
         return EndUserDTO(
             id = entity.id,
-            name=  entity.name,
+//            name=  entity.name,
             email = entity.email,
-            role = Role.valueOf(entity.role.name),
+//            role = Role.valueOf(entity.role.name),
             password = entity.passw
         )
     }
@@ -20,10 +23,10 @@ class EndUserMapper: Mapper<EndUserDTO, EndUser> {
     override fun toEntity(domain: EndUserDTO): EndUser {
         return EndUser(
             id = domain.id,
-            name = domain.name,
-            passw = domain.password,
+            name = "Usuario",
+            passw = passwordEncoder.encode(domain.password),
             email = domain.email,
-            role = domain.role,
+            role = Role.USER,
             joinDate = java.util.Calendar.getInstance(),
             courses = mutableListOf()
         )
