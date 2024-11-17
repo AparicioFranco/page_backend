dde96a9cdcd5cf3646f7af84e20fc33d24f83d82
aparicioFranco <francoapariciom@gmail.com>
2024-11-17 16:54:23 -0300

    Added createGroup

--- src/main/kotlin/apariciomeli/tutorial/kotlinTutorial/service/modulegroup/SetGroupServiceImpl.kt
+++ src/main/kotlin/apariciomeli/tutorial/kotlinTutorial/service/modulegroup/SetGroupServiceImpl.kt
@@ -1,5 +1,7 @@
 package apariciomeli.tutorial.kotlinTutorial.service.modulegroup
 
+import apariciomeli.tutorial.kotlinTutorial.dto.setgroup.GroupDTO
+import apariciomeli.tutorial.kotlinTutorial.mapper.GroupMapper
 import apariciomeli.tutorial.kotlinTutorial.model.SetGroup
 import apariciomeli.tutorial.kotlinTutorial.repo.CourseRepository
 import apariciomeli.tutorial.kotlinTutorial.repo.SetGroupRepository
@@ -9,10 +11,11 @@ import org.springframework.stereotype.Service
 @Service
 class SetGroupServiceImpl(
     private val setGroupRepository: SetGroupRepository,
-    private val courseRepository: CourseRepository
+    private val courseRepository: CourseRepository,
+    private val groupMapper: GroupMapper
 ) : SetGroupService {
 
-  override fun getGroupsByCourseId(courseId: Int): List<SetGroup> {
+    override fun getGroupsByCourseId(courseId: Int): List<SetGroup> {
     val courseOptional = courseRepository.findById(courseId)
     if (courseOptional.isPresent) {
       val course = courseOptional.get()
@@ -20,4 +23,10 @@ class SetGroupServiceImpl(
     }
     throw Exception("Course not found")
   }
+
+    override fun createGroup(setGroup: GroupDTO): GroupDTO {
+        val newGroup = groupMapper.toEntity(setGroup)
+        val savedCourse = setGroupRepository.save(newGroup)
+        return GroupDTO(savedCourse.id,savedCourse.course.id,savedCourse.name)
+    }
 }
