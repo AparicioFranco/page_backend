4692596e1e49655c4d7673a2d03fee9563dc544d
aparicioFranco <francoapariciom@gmail.com>
2025-01-03 16:44:30 -0300

    Change s3config for system.env instead than dotenv

--- src/main/kotlin/apariciomeli/tutorial/kotlinTutorial/config/S3Config.kt
+++ src/main/kotlin/apariciomeli/tutorial/kotlinTutorial/config/S3Config.kt
@@ -1,22 +1,19 @@
 package apariciomeli.tutorial.kotlinTutorial.config
 
-import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.S3Client
-import software.amazon.awssdk.services.s3.S3Configuration
 import java.net.URI
 
 @Configuration
 class S3Config {
-    private final val dotenv: Dotenv = Dotenv.load()
 
-    val accessKey: String = dotenv["AWS_ACCESS_KEY_ID"]
-    val secretKey: String = dotenv["AWS_SECRET_ACCESS_KEY"]
-    val region: String = dotenv["AWS_REGION"]
+    val accessKey: String = System.getenv("AWS_ACCESS_KEY_ID")
+    val secretKey: String = System.getenv("AWS_SECRET_ACCESS_KEY")
+    val region: String = System.getenv("AWS_REGION")
 
     @Bean
     fun s3Client(): S3Client {
